840edc730620b24fcc706cda85fc970587c793e7
he1mant <90799034+he1mant@users.noreply.github.com>
2021-09-30 20:12:15 +0530

    Create day 1&2.py

---
+++ day 1&2.py
@@ -0,0 +1,186 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "id": "204f664d",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "7 hey\n"
+     ]
+    }
+   ],
+   "source": [
+    "x=2\n",
+    "y=5\n",
+    "xy='hey'\n",
+    "print(x+y,xy)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "id": "04160dd0",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "my name is vikas bishnoi I am from punjab\n",
+      "I love python\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"\"\"my name is vikas bishnoi I am from punjab\n",
+    "I love python\"\"\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 6,
+   "id": "81c765ac",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "hello world\n",
+      "hello world !\n"
+     ]
+    }
+   ],
+   "source": [
+    "string1='world'\n",
+    "print('hello',string1)\n",
+    "\n",
+    "string2='!'\n",
+    "print('hello',string1,string2)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 7,
+   "id": "9dc94d0f",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "helloworld!\n"
+     ]
+    }
+   ],
+   "source": [
+    "print('hello'+string1+string2)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 8,
+   "id": "c1289992",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "jan\n",
+      "feb\n",
+      "mar\n",
+      "apr\n",
+      "jun\n",
+      "jul\n",
+      "aug\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"jan\\nfeb\\nmar\\napr\\njun\\njul\\naug\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 9,
+   "id": "f0fe8798",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "I want\\n to be printed.\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"I want\\\\n to be printed.\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 10,
+   "id": "17a4a7f5",
+   "metadata": {},
+   "outputs": [
+    {
+     "ename": "SyntaxError",
+     "evalue": "invalid syntax (3167748265.py, line 10)",
+     "output_type": "error",
+     "traceback": [
+      "\u001b[0;36m  File \u001b[0;32m\"/data/user/0/ru.iiec.pydroid3/cache/ipykernel_31346/3167748265.py\"\u001b[0;36m, line \u001b[0;32m10\u001b[0m\n\u001b[0;31m    percent=90.00\u001b[0m\n\u001b[0m    ^\u001b[0m\n\u001b[0;31mSyntaxError\u001b[0m\u001b[0;31m:\u001b[0m invalid syntax\n"
+     ]
+    }
+   ],
+   "source": [
+    "x=10\n",
+    "print(type(x))\n",
+    "\n",
+    "s='abc'\n",
+    "print(type(s))\n",
+    "\n",
+    "value=true\n",
+    "print(type(value)\n",
+    "\n",
+    "percent=90.00\n",
+    "type(percent)\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "3711602c",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
